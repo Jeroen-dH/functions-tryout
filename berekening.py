7cb413faa6f1487c84d8173ae39f4bca90e29192
Jeroen <99068895@Mydavici.nl>
2021-10-22 11:53:30 +0200

    opdracht 2 af

---
+++ berekening.py
@@ -0,0 +1,23 @@
+def plus(num1,num2):
+    print(num1, "+", num2, "=", num1+num2)
+plus(10,12)
+
+def keer(num1,num2):
+    print(num1, "x", num2, "=", num1*num2)
+keer(6,7)
+
+def min(num1,num2):
+    print(num1, "-", num2, "=", num1-num2)
+min(58,34)
+
+def delen(num1,num2):
+    print(num1, "/", num2, "=", num1/num2)
+delen(144,12)
+
+def plus2(num1,num2):
+    print(num1, "+", num2, "=", num1+num2)
+plus2(12,1)
+
+def min2(num1,num2):
+    print(num1, "-", num2, "=", num1+num2)
+min2(34,1)
